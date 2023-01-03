a65a7c8c40926b909d30b7c30edc6ea88bedd584
Jan Po <tojanpo@gmail.com>
2023-01-03 23:45:32 +1100

    Intial Rest API implemenation with expressjs

---
+++ README.md
@@ -0,0 +1,25 @@
+## RESTful APIs in 100 Seconds // Build an API from Scratch with Node.js Express
+https://www.youtube.com/watch?v=-MTSQjw5DrM&t=432s
+
+
+## Open API Specification
+https://swagger.io/specification/
+
+## Swagger Codegen
+https://swagger.io/tools/swagger-codegen/
+
+```
+npm init -y  
+npm install express
+node .
+```
+
+
+
+## Test
+Insomnia/ Postman
+
+```
+GET http://localhost:8080/tshirt
+POST http://localhost:8080/tshirt/23
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+// const app = require('express')();
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json())
+
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: 't1',
+        size: 'large'
+    })
+});
+
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo!' })
+    }
+
+    res.send({
+        tshirt: `tshirt: tshirt with ${logo} and ID of ${id}`
+    });
+});
+
+
+
+
+app.listen(
+    PORT,
+    () => console.log(`it's alive on http://localhost:${PORT}`)
+)
